0c400bf6ea75bdcedb97fbcce862ec645e5d2ef4
Gustavo Campos <guseducampos@gmail.com>
2018-08-15 16:58:10 -0600

    updated podspec

--- JustNetworking.podspec
+++ JustNetworking.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "JustNetworking"
-  s.version      = "1.0"
+  s.version      = "1.0.1"
   s.summary      = "A small architecture library for simplify the networking layer"
   s.description  = <<-DESC
     Your description here.
---
+++ JustNetworkingTests/HelperForTest.swift
@@ -0,0 +1,84 @@
+//
+//  RequestForTest.swift
+//  JustNetworking-iOS Tests
+//
+//  Created by  LaptopGCampos on 5/18/18.
+//  Copyright © 2018 JustNetworking. All rights reserved.
+//
+
+import Foundation
+import JustNetworking
+
+typealias Token = String
+
+struct User: Decodable {
+    let name: String
+    let lastName: String
+}
+
+enum UserRouter: Router {
+    
+    case user(id: String)
+    
+    var route: Route {
+        switch self {
+        case .user(let id):
+            return Route(path: "/\(id)", method: .get)
+        }
+    }
+}
+
+
+func addHeaders(_ headers: [String: String]) -> RequestBuilder {
+    return { request in
+        var request = request
+        headers.forEach { key, value in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        return request
+    }
+}
+
+func addURLParams(_ dictionary: [String:Any]) -> RequestBuilder {
+    return { request in
+        var request = request
+        let queryItems =  dictionary.map { key, value in
+            return URLQueryItem(name: key, value: "\(value)")
+        }
+        var components = URLComponents(url: request.url!, resolvingAgainstBaseURL: true)
+        components?.queryItems = queryItems
+        request.url = components?.url
+        return request
+    }
+}
+
+func buildRequest<T: Decodable>(type: T.Type,
+                                router: Router,
+                                parameters: [String:Any]? = nil,
+                                requestBuilder: @escaping RequestBuilder) -> BaseRequest<T> {
+    
+    let factory = RequestFactory(router: router,
+                                 requestBuilder: compose(addURLParams(parameters ?? [:]), requestBuilder))
+    
+    return BaseRequest<T>(requestFactory: factory)
+}
+
+func addSecurity(_ token: Token) -> RequestBuilder {
+    return addHeaders(["Authorization": token])
+}
+
+func addContenType(_ content: [String: String]) - > RequestBuilder {
+    return addHeaders(content)
+}
+
+func requestIdentity(_ request: URLRequest) -> URLRequest {
+    return request
+}
+
+
+func securityRequest<T: Decodable>(type: T.Type,
+                                   router: Router,
+                                   parameters: [String:Any]? = nil,
+                                   requestBuilder: @escaping RequestBuilder = requestIdentity) -> BaseRequest<T> {
+    return buildRequest(type: type, router: router, requestBuilder: compose(addSecurity("Bearer 23423"),requestBuilder ))
+}
